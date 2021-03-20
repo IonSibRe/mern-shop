1017cd6a7ec216beda2d0c2fbf7cbea69d7a14b2
IonSibRe <ionsibre@gmail.com>
2021-03-20 20:25:58 +0100

    Initial Commit + Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+	Home,
+	About,
+	Products,
+	Contact,
+	Cart,
+	Login,
+	Register,
+	Checkout,
+	Error,
+} from "./pages";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/about">
+					<About />
+				</Route>
+				<Route path="/products">
+					<Products />
+				</Route>
+				<Route path="/contact">
+					<Contact />
+				</Route>
+				<Route path="/cart">
+					<Cart />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/register">
+					<Register />
+				</Route>
+				<Route path="/checkout">
+					<Checkout />
+				</Route>
+				<Route path="*">
+					<Error />
+				</Route>
+			</Switch>
+		</Router>
+	);
+};
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from "./Home";
+import About from "./About";
+import Products from "./Products";
+import Contact from "./Contact";
+import Cart from "./Cart";
+import Login from "./Login";
+import Register from "./Register";
+import Checkout from "./Checkout";
+import Error from "./Error";
+
+export {
+	Home,
+	About,
+	Products,
+	Contact,
+	Cart,
+	Login,
+	Register,
+	Checkout,
+	Error,
+};
